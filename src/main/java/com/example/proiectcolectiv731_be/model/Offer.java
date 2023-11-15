bcb85969ec4cc2ff513ba379fc192193b76d50f1
gal-emese <114982479+gal-emese@users.noreply.github.com>
2023-11-15 13:10:45 +0200

    Add Offer class

---
+++ src/main/java/com/example/proiectcolectiv731_be/model/Offer.java
@@ -0,0 +1,30 @@
+package com.example.proiectcolectiv731_be.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user_table")
+public class Offer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name", unique = true, nullable = false, length = 20)
+    private String name;
+
+    @Column(name = "details", nullable = false, length = 300)
+    private String details;
+
+    @Column(name = "price", nullable = false)
+    private Long price;
+
+    /*
+    List<String> pictures;
+     */
+}
