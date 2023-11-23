71a9ee2e568d0386f35cb59f7a265511a0e175fe
ComsaAndreea <52425269+ComsaAndreea@users.noreply.github.com>
2023-11-23 17:55:04 +0200

    link change for frontend

--- src/main/java/com/example/proiectcolectiv731_be/controller/UserController.java
+++ src/main/java/com/example/proiectcolectiv731_be/controller/UserController.java
@@ -5,18 +5,17 @@ import com.example.proiectcolectiv731_be.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/users")
 public class UserController {
     @Autowired
     private UserService userService;
 
-    @PostMapping("/users")
+    @PostMapping("/signup")
     public ResponseEntity<String> signUp(@RequestBody UserDto userDto) {
         try {
             userService.signUp(userDto);
