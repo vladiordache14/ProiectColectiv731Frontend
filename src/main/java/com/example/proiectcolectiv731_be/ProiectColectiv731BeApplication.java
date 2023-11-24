85f6c9e9e13d08fd69616e93ec9e8cb1bcc72081
Radu <radustefanboian@gmail.com>
2023-11-24 11:06:57 +0200

    Random pass generator

--- src/main/java/com/example/proiectcolectiv731_be/ProiectColectiv731BeApplication.java
+++ src/main/java/com/example/proiectcolectiv731_be/ProiectColectiv731BeApplication.java
@@ -1,11 +1,15 @@
 package com.example.proiectcolectiv731_be;
 
+import com.example.proiectcolectiv731_be.pass_reset.GMailer;
+import com.example.proiectcolectiv731_be.pass_reset.RandomPasswordGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class  ProiectColectiv731BeApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ProiectColectiv731BeApplication.class, args);
     }
--- src/main/java/com/example/proiectcolectiv731_be/pass_reset/GMailer.java
+++ src/main/java/com/example/proiectcolectiv731_be/pass_reset/GMailer.java
@@ -1,7 +1,21 @@
 package com.example.proiectcolectiv731_be.pass_reset;
 
-public class GMailer {
-    public void sendMail(String subject,String message, String destination){
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 
+@Service
+public class GMailer {
+    @Autowired
+    private JavaMailSender mailSender;
+    public void sendMail(String subject,String message, String destination){
+        SimpleMailMessage mail=new SimpleMailMessage();
+        mail.setFrom("cardealership731@gmail.com");
+        mail.setTo(destination);
+        mail.setText(message);
+        mail.setSubject(subject);
+
+        mailSender.send(mail);
     }
 }
